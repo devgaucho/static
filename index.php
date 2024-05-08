bbddd201073b9122e4e3a920e1d9013093dc3d53
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-05-08 19:26:14 -0300

    arquivos do guest

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Site: guest
+ * Date: 08/05/24
+ * Time: 18:55
+ */
+require 'public/index.php';
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Site: guest
+ * Date: 08/05/24
+ * Time: 18:44
+ */
+print 'hello guest';
